01ffbbe3a7290ace083cc46461688e3e7ccb3171
Andreas Lind <andreas@one.com>
2017-03-16 23:02:50 +0100

    Adapt to the feature/hook branch of unexpected.

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
-var consider = require('./lib/consider')(require('unexpected'));
+const expect = require('unexpected').clone().use(require('./lib/consider'));
 
-consider.addAssertion('<any> when delayed a bit <assertion>', function (expect, ...rest) {
+expect.addAssertion('<any> when delayed a bit <assertion>', function (expect, ...rest) {
     return expect.promise(function (run) {
         setTimeout(run(function () {
             return expect(...rest);
@@ -10,20 +10,20 @@ consider.addAssertion('<any> when delayed a bit <assertion>', function (expect, 
 
 describe('consider', () => {
     it('should fail', () => {
-        consider(1, 'to equal', 2);
-        consider('abc', 'to equal', 'abc');
-        consider(2, 'to equal', 3);
+        expect(1, 'to equal', 2);
+        expect('abc', 'to equal', 'abc');
+        expect(2, 'to equal', 3);
     });
 
     it('should succeed', () => {
-        consider(1, 'to equal', 1);
-        consider('abc', 'to equal', 'abc');
-        consider(2, 'to equal', 2);
+        expect(1, 'to equal', 1);
+        expect('abc', 'to equal', 'abc');
+        expect(2, 'to equal', 2);
     });
 
     it('should work with async assertions', async () => {
-        consider('abc', 'when delayed a bit to equal', 'abe');
-        let captures = await consider('abc', 'when delayed a bit to match', /^(a)/);
-        consider(captures, 'to satisfy', {index: 0});
+        expect('abc', 'when delayed a bit to equal', 'abe');
+        let captures = await expect('abc', 'when delayed a bit to match', /^(a)/);
+        expect(captures, 'to satisfy', {index: 0});
     });
 });
