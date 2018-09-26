281bde0119a13d30dfa9ee56193eac115916edc3
Andreas Lind <andreaslindpetersen@gmail.com>
2018-09-26 20:13:18 +0200

    eslint --fix .

--- test.js
+++ test.js
@@ -1,29 +1,37 @@
-const expect = require('unexpected').clone().use(require('./lib/consider'));
+const expect = require('unexpected')
+  .clone()
+  .use(require('./lib/consider'));
 
-expect.addAssertion('<any> when delayed a bit <assertion>', function (expect, ...rest) {
-    return expect.promise(function (run) {
-        setTimeout(run(function () {
-            return expect(...rest);
-        }), 10);
-    });
+expect.addAssertion('<any> when delayed a bit <assertion>', function(
+  expect,
+  ...rest
+) {
+  return expect.promise(function(run) {
+    setTimeout(
+      run(function() {
+        return expect(...rest);
+      }),
+      10
+    );
+  });
 });
 
 describe('consider', () => {
-    it('should fail', () => {
-        expect(1, 'to equal', 2);
-        expect('abc', 'to equal', 'abc');
-        expect(2, 'to equal', 3);
-    });
+  it('should fail', () => {
+    expect(1, 'to equal', 2);
+    expect('abc', 'to equal', 'abc');
+    expect(2, 'to equal', 3);
+  });
 
-    it('should succeed', () => {
-        expect(1, 'to equal', 1);
-        expect('abc', 'to equal', 'abc');
-        expect(2, 'to equal', 2);
-    });
+  it('should succeed', () => {
+    expect(1, 'to equal', 1);
+    expect('abc', 'to equal', 'abc');
+    expect(2, 'to equal', 2);
+  });
 
-    it('should work with async assertions', async () => {
-        expect('abc', 'when delayed a bit to equal', 'abe');
-        let captures = await expect('abc', 'when delayed a bit to match', /^(a)/);
-        expect(captures, 'to satisfy', {index: 0});
-    });
+  it('should work with async assertions', async () => {
+    expect('abc', 'when delayed a bit to equal', 'abe');
+    let captures = await expect('abc', 'when delayed a bit to match', /^(a)/);
+    expect(captures, 'to satisfy', { index: 0 });
+  });
 });
